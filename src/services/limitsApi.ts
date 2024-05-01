6aba056baddad4e3d455a4fbe0bf2fadd0281b5c
ySnoopyDogy <CommandPlayermine@gmail.com>
2024-05-01 20:50:36 -0300

    feat: use hardcoded limits to allow deploy

--- src/services/limitsApi.ts
+++ src/services/limitsApi.ts
@@ -1,4 +1,6 @@
+import { AxiosError } from 'axios';
 import { Endpoints, MayBeError, apiRequest } from '.';
+import logger from './logger';
 
 export type Limits = {
   retrospective: {
@@ -14,7 +16,19 @@ export type Limits = {
 };
 
 const getLimits = async (): Promise<MayBeError<Limits>> => {
-  const result = await apiRequest.get(Endpoints.Limits).catch(() => null);
+  const result = await apiRequest.get(Endpoints.Limits).catch((e: AxiosError) => {
+    if (e?.response?.status === 404) {
+      logger.error('The limit route is not yet implemented. Using hardcoded ones');
+
+      return {
+        data: {
+          answer: { text: 300 },
+          question: { text: 100 },
+          retrospective: { name: 100, description: 300 },
+        },
+      };
+    }
+  });
 
   if (!result) return { error: true };
 
